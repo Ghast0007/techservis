bd188ab152a70c785385d0e9c4ffe409cf94bde1
Damian Dobrzyński <21025@mazowiecka.edu.pl>
2021-11-12 17:57:00 +0100

    part 1
    part 1

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    <h1> serwis komputerowy </h1> 
+
+    <div className="form">
+      <label>Nick:</label>
+      <input type="text" name="Nick" />
+      <label>Hasło:</label>
+      <input type="text" name="Hasło" />
+
+      <button>Zaloguj</button>
+
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
