b620a4a85993c38eaf3991703bb1a6d468aa543a
Damian Dobrzyński <21025@mazowiecka.edu.pl>
2021-11-23 21:57:03 +0100

    register/login
    rejestracja logowanie

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,78 @@
 import './App.css';
+import React, { useState} from "react";
+import Axios from 'axios';
 
 function App() {
+
+  const [loginREG, setloginREG] = useState("");
+  const [hasloREG, sethasloREG] = useState("");
+
+  const [login, setloginLOG] = useState("");
+  const [haslo, sethasloLOG] = useState("");
+
+  const [loginStatus, setloginStatus] = useState("");
+
+
+  const zarejestruj = () => {
+
+    Axios.post("http://localhost:3001/api/insert", {
+      login: loginREG,
+      haslo: hasloREG,
+    }).then(()=> {
+      alert("succesful insert");
+    });
+  };
+
+  const zaloguj = () => {
+
+    Axios.post("http://localhost:3001/api/login", {
+      login: login,
+      haslo: haslo,
+    }).then((response)=> {
+      if (response.data.message) {
+        setloginStatus(response.data.message);
+      }else
+      {
+        setloginStatus(response.data[0].login);
+      }
+    });
+  };
   return (
     <div className="App">
      
-    <h1> serwis komputerowy </h1> 
+    <h1> Zarejestruj się </h1>
 
     <div className="form">
-      <label>Nick:</label>
-      <input type="text" name="Nick" />
-      <label>Hasło:</label>
-      <input type="text" name="Hasło" />
+      
+      <input type="email" name="login" placeholder="E-mail ..." onChange={(e)=> {
+        setloginREG(e.target.value);
+      }}/>
+      
+      <input type="text" name="haslo" placeholder="Hasło ..." onChange={(e)=> {
+        sethasloREG(e.target.value);
+      }}/>
 
-      <button>Zaloguj</button>
+      <button onClick={zarejestruj}>Zarejestruj</button>
 
     </div>
+
+    <h1> Zaloguj się </h1>
+
+    <div className="form">
+      
+      <input type="text" name="login" placeholder="E-mail ..." onChange={(e)=> {
+        setloginLOG(e.target.value);
+      }}/>
+      
+      <input type="text" name="haslo" placeholder="Hasło ..." onChange={(e)=> {
+        sethasloLOG(e.target.value);
+      }}/>
+
+      <button onClick={zaloguj}>Zaloguj</button>
+
+    </div>
+
+    <h1>{ loginStatus }</h1>
 
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,8 +1,56 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+const mysql = require('mysql');
 
-app.get("/", (req, res) =>{
-    res.send("hello world");
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password:"",
+    database:"techservisDATABASE",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.post("/api/insert", (req, res) => {
+
+    const login = req.body.login;
+    const haslo = req.body.haslo;
+
+
+    const sqlInsert =
+     "INSERT INTO logowanie (login, haslo) VALUES (?,?)";
+    db.query(sqlInsert, [login, haslo], (err, result) => {
+        console.log(result);
+        
+    });
+});
+
+app.post("/api/login", (req, res) => {
+
+    const login = req.body.login;
+    const haslo = req.body.haslo;
+
+    db.query(
+        "SELECT * FROM logowanie WHERE login = ? AND haslo = ?",
+        [login, haslo],
+        (err, result) => {
+            if (err){
+                res.send({err: err});
+            }
+            if (result.length > 0) {
+                res.send (result);
+
+            }else {
+                res.send({message: "Nie znaleziono użytkownika"});
+            }
+        }
+    );
 });
 
 app.listen(3001, () => {
