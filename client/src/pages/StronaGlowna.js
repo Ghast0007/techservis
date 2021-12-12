69828bdca8a503f7eb1515d8c46ce9ab58b91901
Damian Dobrzyński <21025@mazowiecka.edu.pl>
2021-12-12 20:58:59 +0100

    slider

--- client/src/pages/StronaGlowna.js
+++ client/src/pages/StronaGlowna.js
@@ -1,22 +1,50 @@
 import React from "react";
 import '../App.css';
+import { Carousel } from "react-bootstrap";
+import image1 from "../images/image1.jpg";
+import image2 from "../images/image2.jpg";
+import image3 from "../images/image3.jpg";
 
-
-function StronaGlowna() {
+const StronaGlowna = () => {
     return (
-    
-    <div  className="StronaGlowna">
-        
-        <h1><center>Strona głowna</center></h1>
-        <center>
-    <p/>
-    <a href="/login">Zaloguj się</a>
-    <p/>
-    <a href="/register">Zarejestruj się</a>
-    
-    </center>
-    </div>
+        <div className="slider">
+        <Carousel variant="dark" interval="4000">
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={image1}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={image2}
+      alt="Second slide"
+    />
+    <Carousel.Caption>
+     
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={image3}
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</div>
     )
+    
+   
+    
 }
 
 export default StronaGlowna;
