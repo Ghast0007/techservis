0dbfc8b3c4f60b1c17dcb455beac81f63f0d0700
Damian Dobrzyński <21025@mazowiecka.edu.pl>
2022-03-22 19:28:10 +0100

    Finish

--- baza/techservisdatabase (5).sql
+++ baza/techservisdatabase (5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 10 Mar 2022, 22:15
+-- Czas generowania: 22 Mar 2022, 19:27
 -- Wersja serwera: 10.4.21-MariaDB
 -- Wersja PHP: 8.0.11
 
@@ -45,14 +45,15 @@ CREATE TABLE `logowanie` (
 INSERT INTO `logowanie` (`Id`, `login`, `haslo`, `rola`, `imie`, `nazwisko`, `mail`, `telefon`) VALUES
 (30, 'Dorotka', '$2b$10$Au90aXnJk/nSrXhKY7E8qeEHr33ttMac/1QIbJ6YHoeyIFOTu4A4m', 'user', 'Dorota', 'Olszewska', 'ghast0007@gmail.com', '321654987'),
 (31, 'Ghast0007', '$2b$10$O.SkWgRQnwb3fJpMLNDYze53HfF7YN9ssOLZ2FeVvj2Z7upjc2Q1u', 'user', 'Damian', 'Dobrzyński', 'ghast0007@gmail.com', '654654654'),
-(32, 'admin', '$2b$10$YSKAzEGf8B5lGIYpkr/gvuO0n4WqKAlRTn2lvIl7v9EWcho3oeQmq', 'admin', 'Damian', 'Dobrzyński', 'ghast0007@gmail.com', '654654654'),
 (34, 'natali', '$2b$10$kicP3CI8Ac.E5SNRYyWwNu6AJRog36SAThv72Glc07Bt8GWSDFIPu', 'user', 'natalia', 'dobrzyńska', 'cos@mail.pl', '234346468'),
 (35, 'Kotek', '$2b$10$Fu4h6AXAzWyCXYgRsrlXOeJEzlIU3GdWZG8AwpSy5GHEF/UebUn1q', 'user', 'Ola', 'Kowalska', 'ola@mail.com', '123456789'),
 (36, 'szarik', '$2b$10$YR/mrJeVNIR9sTuUx6JOIuHcnSLxeFvZudxHwdQ40MjTOBOM/UwnK', 'user', 'szarik', 'nowak', 'szarik@mail.com', '123321123'),
 (39, 'Jan', '$2b$10$S.XpWAriA0Wp5j12WKHZ0.YRSYNrYKQzXArAa4Vbvd1k2yriLxtpO', 'user', 'Jan', 'Kowalski', 'jan@onet.pl', '1236654978'),
 (44, 'Puszek', '$2b$10$I3ZafecW8gXvzWDZSNWtFOuYUgNqKPO6BR3GBr8xy5yBCsDo8Qu.C', 'user', 'Kamil', 'Nowak', 'nowak@mail.com', '+48 518 477 556'),
 (47, 'Maciek', '$2b$10$I.zkuXfM6kqZpFxf.q9q9ewacA.efURQpMSqhoBJkdCYPEKNPFhdK', 'user', 'Maciek', 'Jakiś', 'maciek@mail.vom', '+48 518 477 006'),
-(48, 'Marcel123', '$2b$10$7qEg2zTeUNsvIS5MgfknJu3I5TQ7XRdsF3H0yEOF/CNpZL2ScUPUi', 'user', 'Marcel', 'Szeluga', 'natalka12042004@wp.pl', '+48 789 254 062');
+(48, 'Marcel123', '$2b$10$7qEg2zTeUNsvIS5MgfknJu3I5TQ7XRdsF3H0yEOF/CNpZL2ScUPUi', 'user', 'Marcel', 'Szeluga', 'natalka12042004@wp.pl', '+48 789 254 062'),
+(49, 'Kasia', '$2b$10$TVbydJLaDWWn/mK18N4u2eK2XUbNkUrTCJhfhBPvlLqwWHVs4V8lm', 'user', 'Kasia', 'Nowicka', 'kasia@onet.pl', '654987321'),
+(50, 'Admin', '$2b$10$wMh6qKgJVPilmFO25goCWu9iqMMzCsOT1rFvVfNCSyjCKd8IyzTpS', 'admin', 'Damian', 'Dobrzyński', 'ghast0007@gmail.com', '+48 506 527 440');
 
 -- --------------------------------------------------------
 
@@ -64,7 +65,7 @@ CREATE TABLE `zamowienia` (
   `id_zamowienia` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `opis` text NOT NULL,
-  `url` varchar(255) NOT NULL,
+  `user_img` varchar(255) NOT NULL,
   `kategoria` varchar(255) NOT NULL,
   `czy_zrealizowane` int(1) NOT NULL,
   `czy_zaplacone` int(1) NOT NULL,
@@ -75,9 +76,11 @@ CREATE TABLE `zamowienia` (
 -- Zrzut danych tabeli `zamowienia`
 --
 
-INSERT INTO `zamowienia` (`id_zamowienia`, `user_id`, `opis`, `url`, `kategoria`, `czy_zrealizowane`, `czy_zaplacone`, `data`) VALUES
+INSERT INTO `zamowienia` (`id_zamowienia`, `user_id`, `opis`, `user_img`, `kategoria`, `czy_zrealizowane`, `czy_zaplacone`, `data`) VALUES
 (37, 31, 'Chciałbym wymienić dysk hdd na ssd w laptopie najlepiej na 1TB.', '', 'Laptopy', 1, 1, '2022-03-09 23:11:05'),
-(38, 31, 'Chciałbym wymienić procesor na i5 12600k oraz płytę główną i chłodzenie procesora', '', 'Komputery', 0, 0, '2022-03-09 23:14:02');
+(38, 31, 'Chciałbym wymienić procesor na i5 12600k oraz płytę główną i chłodzenie procesora', '', 'Komputery', 0, 0, '2022-03-09 23:14:02'),
+(54, 49, 'Zalałam laptopa, nie włącza się ', '1647880704650-zalany.jpg', 'Laptopy', 1, 0, '2022-03-21 17:47:49'),
+(55, 49, 'Matryca w monitorze mi się rozlała chciałbym wymienić matrycę na nową', '1647883164770-matryca.jpg', 'Inne', 1, 0, '2022-03-21 17:37:21');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -103,13 +106,13 @@ ALTER TABLE `zamowienia`
 -- AUTO_INCREMENT dla tabeli `logowanie`
 --
 ALTER TABLE `logowanie`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT dla tabeli `zamowienia`
 --
 ALTER TABLE `zamowienia`
-  MODIFY `id_zamowienia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id_zamowienia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
